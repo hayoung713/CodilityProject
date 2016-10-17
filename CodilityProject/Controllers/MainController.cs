fdc2ea286740f3cc51c37a257164d25f23a0a0dc
hayoung713 <hayoung713@naver.com>
2016-10-17 14:32:20 +0900

    기본 controller 및 view 추가
    기본 controller 및 view 추가

---
+++ CodilityProject/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodilityProject.Controllers
+{
+    [RoutePrefix("")]
+    public class MainController : CommonController
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("Lesson1")]
+        public ActionResult Lesson1()
+        {
+            return View();
+        }
+    }
+}
