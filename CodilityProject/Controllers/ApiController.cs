7897fb92a5d673e194372262ecf7e9b3fbf005f4
hayoung713 <hayoung713@naver.com>
2016-10-18 13:45:59 +0900

    import
    import

--- CodilityProject/Controllers/ApiController.cs
+++ CodilityProject/Controllers/ApiController.cs
@@ -16,33 +16,30 @@ namespace CodilityProject.Controllers
             int N = Convert.ToInt32(Request.Form["number"]);
             int longestBinaryGap = 0;
 
-            if (N > 0 && N <= 2147483647)
+            // N을 2진수 문자열로 변환
+            string repStr = Convert.ToString(N, 2);
+            string[] repArr = repStr.Select(r => r.ToString()).ToArray();
+
+            int tempLongestBinary = 0;
+
+            // 1에 해당하는 자리수를 gap array 에 저장
+            for (int i = 0; i < repArr.Length; i++)
             {
-                // N을 2진수 문자열로 변환
-                string repStr = Convert.ToString(N, 2);
-                string[] repArr = repStr.Select(r => r.ToString()).ToArray();
-
-                int tempLongestBinary = 0;
-
-                // 1에 해당하는 자리수를 gap array 에 저장
-                for (int i = 0; i < repArr.Length; i++)
+                if (repArr[i].Equals("1"))
                 {
-                    if (repArr[i].Equals("1"))
+
+                    // 첫 1이 아니며, 이전 1과 연속되지 않으며, 
+                    if (tempLongestBinary != 0 && tempLongestBinary != i && (i - tempLongestBinary) > longestBinaryGap)
                     {
-
-                        // 첫 1이 아니며, 이전 1과 연속되지 않으며, 
-                        if (tempLongestBinary != 0 && tempLongestBinary != i && (i - tempLongestBinary) > longestBinaryGap)
-                        {
-                            // 이전 1과 현재 i 와의 갭이 가장 큰 갭이 된다.
-                            longestBinaryGap = i - tempLongestBinary;
-                        }
-
-                        tempLongestBinary = i + 1;
+                        // 이전 1과 현재 i 와의 갭이 가장 큰 갭이 된다.
+                        longestBinaryGap = i - tempLongestBinary;
                     }
+
+                    tempLongestBinary = i + 1;
                 }
             }
 
-            return Json(new { binaryGap = longestBinaryGap }, JsonRequestBehavior.AllowGet);
+            return Json(new { inputNumber = N, binaryNumber = repStr, binaryGap = longestBinaryGap }, JsonRequestBehavior.AllowGet);
         }
     }
 }
