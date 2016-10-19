69fd98c41da60b81b486a4d557fd6784423eab4a
hayoung713 <hayoung713@naver.com>
2016-10-19 14:36:39 +0900

    기본 jquery + ajax 추가
    기본 jquery + ajax 추가

--- CodilityProject/Controllers/ApiController.cs
+++ CodilityProject/Controllers/ApiController.cs
@@ -9,7 +9,7 @@ namespace CodilityProject.Controllers
     [RoutePrefix("api")]
     public class ApiController : CommonController
     {
-        [Route("L1T1")]
+        [Route("L1T1"), HttpPost]
         public JsonResult L1T1()
         {
             // write your code in C# 6.0 with .NET 4.5 (Mono)
